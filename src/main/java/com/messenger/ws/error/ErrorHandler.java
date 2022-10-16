5435affac9f4734fcfa4a98079bbdfec50c33dd9
Yasin <yasin.ekinci@hotmail.com>
2022-10-16 18:33:54 +0200

    ws

--- src/main/java/com/messenger/ws/error/ErrorHandler.java
+++ src/main/java/com/messenger/ws/error/ErrorHandler.java
@@ -1,5 +1,7 @@
 package com.messenger.ws.error;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.error.ErrorAttributeOptions.Include;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
@@ -26,8 +29,17 @@ public class ErrorHandler implements ErrorController {
 		String message = (String) attributes.get("message");
 		String path = (String) attributes.get("path");
 		int status = (Integer) attributes.get("status");
-	
-		return new ApiError(status, message, path);
+		ApiError error = new ApiError(status, message, path);
+		if(attributes.containsKey("errors")) {
+			@SuppressWarnings("unchecked")
+			List<FieldError> fieldErrors = (List)attributes.get("errors");
+			Map<String, String> validationErrors = new HashMap<>();
+			for(FieldError fieldError: fieldErrors) {
+				validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+			}
+			error.setValidationErrors(validationErrors);
+		}
+		return error; 
 	}
 	
 	
